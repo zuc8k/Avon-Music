7bc29c69c7f21f8010dec7bf41388b4e768d4c34
Boody Zuckerberg <abdohassan9090@icloud.com>
2025-12-24 11:49:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const client = require("./client");
 const { token } = require("./config");
 
+// Handlers
 require("./handlers/slashHandler")(client);
 require("./handlers/prefixHandler")(client);
 require("./handlers/interactionHandler")(client);
 require("./handlers/buttonHandler")(client);
 
-require("./player/events")(client); // ✅ جديد
+// Player Events
+require("./player/events")(client);
+
+// Auto UI Update (Progress Bar)
+require("./player/autoUpdate")(client);
 
 client.once("ready", () => {
   console.log(`✅ Logged in as ${client.user.tag}`);
