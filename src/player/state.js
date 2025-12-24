7350217507fa10e487e26b268aab58b1cc4c35f0
Boody Zuckerberg <abdohassan9090@icloud.com>
2025-12-24 11:33:19 +0300

    Update state.js

--- src/player/state.js
+++ src/player/state.js
@@ -1,8 +1,19 @@
-const panels = new Map();
-/*
- panels.set(guildId, {
-   channelId,
-   messageId
- })
-*/
-module.exports = { panels };
+class PanelState {
+  constructor() {
+    this.panels = new Map();
+  }
+
+  set(guildId, data) {
+    this.panels.set(guildId, data);
+  }
+
+  get(guildId) {
+    return this.panels.get(guildId) || null;
+  }
+
+  delete(guildId) {
+    this.panels.delete(guildId);
+  }
+}
+
+module.exports = new PanelState();
