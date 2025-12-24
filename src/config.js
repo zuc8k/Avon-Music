6e25feda102de04113e91a0dc253c2712c284038
Boody Zuckerberg <abdohassan9090@icloud.com>
2025-12-24 15:06:30 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,11 @@
 module.exports = {
   PREFIX: "!",
 
+  BOT_NAME: "Luna Music Bot",
+  VERSION: "1.0.0",
+
+  SUPPORT_SERVER: "https://discord.gg/PUT_SUPPORT_SERVER",
+
   OWNERS: [
     "PUT_YOUR_DISCORD_ID"
   ],
