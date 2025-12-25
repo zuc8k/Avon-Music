13ae0013c90311fb3e4f57cbab3a1beacf39857e
Boody Zuckerberg <abdohassan9090@icloud.com>
2025-12-25 20:39:29 +0300

    Remove unnecessary comments and clean up code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 const client = require("./client");
 const { token } = require("./config");
 
-// Handlers
 require("./handlers/slashHandler")(client);
 require("./handlers/prefixHandler")(client);
 require("./handlers/interactionHandler")(client);
 require("./handlers/buttonHandler")(client);
 require("./handlers/helpMenuHandler")(client);
 
-// Player Events
 require("./player/events")(client);
-
-// UI Auto Update
 require("./player/autoUpdate")(client);
-
-// Prime Auto Expire
 require("./premium/autoExpire")(client);
 
 client.once("ready", () => {
