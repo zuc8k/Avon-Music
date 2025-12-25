2ec779a3f8e322fb04187aee086c6cded1040c18
Boody Zuckerberg <abdohassan9090@icloud.com>
2025-12-25 20:40:37 +0300

    Initialize Discord client with intents and partials

---
+++ src/client.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.Reaction
+  ]
+});
+
+module.exports = client;
